54fa2f7fdd5a3d43c58c6c2ff9ce76ada66b062b
Byron Ponce <bp@headwire.com>
2021-07-21 00:06:45 -0700

    use youtube embed instead of html5 video (mp4)

--- src/components/video/index.svelte
+++ src/components/video/index.svelte
@@ -8,6 +8,12 @@
 
 <div class="video-hero">
 	<div class="ar-16-9">
-		<div class="video"></div>
+		<iframe 
+			class="video" 
+			src="https://www.youtube-nocookie.com/embed/IbGTsoLsNfk?controls=0" 
+			title="Wax Me Boutique Video" 
+			frameborder="0" 
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+			allowfullscreen></iframe>
 	</div>
 </div>
