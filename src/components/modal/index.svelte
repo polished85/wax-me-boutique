8ae675a36d8d5da9b8367c09de4d4822aa9b9e09
Byron Ponce <bp@headwire.com>
2021-07-21 00:05:44 -0700

    add modal component

---
+++ src/components/modal/index.svelte
@@ -0,0 +1,16 @@
+<style lang="scss" global>
+	@import "styles.scss";
+</style>
+
+<script>
+	import { XIcon } from 'svelte-feather-icons';
+	export let modalOpen 
+	export let closeModal
+</script>
+
+<div class="modal {modalOpen ? 'open' : ''}" on:click={closeModal}>
+	<div class="modal-content">
+		<button class="close" on:click={closeModal}><XIcon size="24"/></button>
+		<slot></slot>
+	</div>
+</div>
