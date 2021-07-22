7c3e9c0a1096119f983c243a6c18a1763883acec
Byron Ponce <bp@headwire.com>
2021-07-22 02:49:03 -0700

    make family image full width

--- src/components/about/index.svelte
+++ src/components/about/index.svelte
@@ -7,13 +7,10 @@
 </script>
 
 <div class="about">
+	<img src="{data.image}" />
+
 	<div class="container">
 		<h2>{data.title}</h2>
-		<div class="image">
-			<div class="ar-16-9">
-				<img src="{data.image}" />
-			</div>
-		</div>
 		<div class="content">
 			{#each data.text as text, i}
 				<p>{text}</p>
