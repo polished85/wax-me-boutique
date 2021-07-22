2679fed125252e5905301aeed2482576eaced31e
Byron Ponce <bp@headwire.com>
2021-07-22 02:49:51 -0700

    add autoplay to video

--- src/components/video/index.svelte
+++ src/components/video/index.svelte
@@ -10,7 +10,7 @@
 	<div class="ar-16-9">
 		<iframe 
 			class="video" 
-			src="https://www.youtube-nocookie.com/embed/IbGTsoLsNfk?controls=0" 
+			src="https://www.youtube-nocookie.com/embed/IbGTsoLsNfk?controls=1&autoplay=1&mute=1" 
 			title="Wax Me Boutique Video" 
 			frameborder="0" 
 			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
