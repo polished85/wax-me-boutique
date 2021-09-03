91892c85a16caa0c7b4812a7879b575df1caa436
Byron Ponce <bp@headwire.com>
2021-09-02 23:31:36 -0700

    add reusable image component

---
+++ src/components/image/index.svelte
@@ -0,0 +1,13 @@
+<style lang="scss">
+	@import "styles.scss";
+</style>
+
+<script>
+	export let data;
+</script>
+
+<div class="image">
+	<figure class="ar-{data.ar} fit-{data.fit}">
+		<img width="{data.width}" height="{data.height}" src="{data.path}" />
+	</figure>
+</div>
