3689af71d244ac2e6deaa3cfd913eb2dcd00ee6a
Byron Ponce <byron@polished-web.com>
2022-01-02 20:12:49 -0800

    Implement EmbdedSocial Instagram widget

--- src/components/instagram/index.svelte
+++ src/components/instagram/index.svelte
@@ -3,24 +3,25 @@
 </style>
 
 <script>
-	let data = [1,2,3,4,5,6,7,8]
-	let active = 0
+	import { onMount } from 'svelte';
+
+	function addEmbedSocialScript (d, s, id){
+		var js; 
+		if (d.getElementById(id)) {return;} 
+		js = d.createElement(s); 
+		js.id = id; 
+		js.src = "https://embedsocial.com/cdn/ht.js"; 
+		d.getElementsByTagName("head")[0].appendChild(js);
+	}
+
+	onMount(() => {
+		addEmbedSocialScript(document, 'script', 'EmbedSocialHashtagScript')
+	})
 </script>
 
 <div class="instagram">
 	<div class="container">
 		<h2>Instagram Feed</h2>
-		<div class="row">
-			{#each data as index, i}
-				<div class="col-12 col-md-6 col-lg-4 col-xl-3">
-					<div class="image">
-						<img src="/img/ig-{index}.jpg" />
-						<div class="hover-content">
-							<p>Instagram Caption {index}</p>
-						</div>
-					</div>
-				</div>
-			{/each}
-		</div>
+		<div class="embedsocial-hashtag" data-ref="be47900049f42ce9baa7ebb4a961df2c4523be0c"></div>
 	</div>
 </div>
