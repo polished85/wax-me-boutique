9972f1e0fc9c029b9e235d53ee88f19be6c63932
Byron Ponce <byron@polished-web.com>
2022-01-02 22:44:29 -0800

    Use custom CSS to crop bottom images from IG feed

--- src/components/instagram/index.svelte
+++ src/components/instagram/index.svelte
@@ -2,26 +2,11 @@
 	@import "styles.scss";
 </style>
 
-<script>
-	import { onMount } from 'svelte';
-
-	function addEmbedSocialScript (d, s, id){
-		var js; 
-		if (d.getElementById(id)) {return;} 
-		js = d.createElement(s); 
-		js.id = id; 
-		js.src = "https://embedsocial.com/cdn/ht.js"; 
-		d.getElementsByTagName("head")[0].appendChild(js);
-	}
-
-	onMount(() => {
-		addEmbedSocialScript(document, 'script', 'EmbedSocialHashtagScript')
-	})
-</script>
-
 <div class="instagram">
 	<div class="container">
 		<h2>Instagram Feed</h2>
-		<div class="embedsocial-hashtag" data-ref="be47900049f42ce9baa7ebb4a961df2c4523be0c"></div>
+		<div class="iframe-wrap">
+			<iframe scrolling=no src='https://embedsocial.com/facebook_album/pro_hashtag/be47900049f42ce9baa7ebb4a961df2c4523be0c'></iframe>
+		</div>
 	</div>
 </div>
