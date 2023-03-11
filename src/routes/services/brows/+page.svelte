6a3f942daccdf67ae37674d6a4f3eec1b9380fb0
Byron Ponce <byron@polished-web.com>
2023-03-10 19:50:23 -0800

    Add brows page with unique markup

---
+++ src/routes/services/brows/+page.svelte
@@ -0,0 +1,67 @@
+<script>
+	import Image from '$lib/image/index.svelte';
+	import Hero from '$lib/hero/index.svelte';
+	import Reservation from '$lib/reservation/index.svelte';
+	export let data;
+</script>
+
+<style lang="scss">
+	@import '$lib/../style/variables.scss';
+	h1 {
+		margin-bottom: 6rem;
+	}
+	.main {
+		padding: 6rem 0;
+	}
+	.section {
+		margin-bottom: 6rem;
+	}
+
+	
+
+	@media(max-width: $screen-lg-down){
+
+	}
+	@media(min-width: $screen-xl-up){
+		h2.title {
+			text-align: right;
+			&:after {
+				margin-right: 0;
+			}
+		}
+		.text {
+			padding-left: 2rem;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>{data.head.title}</title>
+	<meta name="description" content="{data.head.description}">
+	<meta name="keywords" content="{data.head.keywords}">
+	<meta name="author" content="{data.head.author}">
+</svelte:head>
+
+<Hero data={data.hero}/>
+
+<main class="main">
+	<div class="container">
+		<h1>{data.main.title}</h1>
+		{#each data.main.sections as section, i}
+			<div class="section">
+				{#each section.text as text, i}
+					<div class="row">
+						<div class="col-12 col-xl-4">
+							<h2 class="title">{section.title}</h2>
+						</div>
+						<div class="col-12 col-xl-8">
+							<p class="text">{@html text}</p>
+						</div>
+					</div>
+				{/each}
+			</div>
+		{/each}
+	</div>
+</main>
+
+<Reservation></Reservation>
