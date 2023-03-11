6e4a94b7b807d0e45e22ccfe3164916a4a16a4cf
Byron Ponce <byron@polished-web.com>
2023-03-10 19:50:05 -0800

    Add hero component

---
+++ src/components/hero/index.svelte
@@ -0,0 +1,27 @@
+<style lang="scss" global>
+	@import "./styles.scss";
+</style>
+
+<script>
+	export let data;
+</script>
+
+<header class="hero overlay">
+	{#if data.image}
+		<div class="hero-image ar-{data.image.ar}" style="background-image: url({data.image.path})"></div>
+	{/if}
+	{#if data.overlay}
+		<div class="overlay" style="opacity: {data.overlay};"></div>
+	{/if}
+	<div class="content">
+		<div class="container">
+			{#if data.title}
+				<div class="title">{data.title}</div>
+			{/if}
+			{#if data.cta}
+				<a href="{data.cta.url}" class="btn-primary dark">{data.cta.text}</a>
+			{/if}
+		</div>
+	</div>
+</header>
+
