5c8c3c37931792287996b6d32b133b4f2270dcd5
Byron Ponce <byron@polished-web.com>
2023-03-18 02:06:50 -0700

    Add service list component (used in error page and services page)

---
+++ src/components/services/index.svelte
@@ -0,0 +1,23 @@
+<style lang="scss" global>
+	@import "./styles.scss";
+</style>
+
+<script>
+	export let data;
+</script>
+
+<section class="services-list">
+	<div class="container">
+		<h1>{data.title}</h1>
+		<ul>
+			{#each data.links as link}
+				{#if link.type === 'dropdown'}
+					{#each link.links as service}
+						<li><a rel='{service.external ? "external" : ""}' href='{service.url}'>{service.name}</a></li>
+					{/each}
+				{/if}
+			{/each}
+		</ul>	
+	</div>
+</section>
+
