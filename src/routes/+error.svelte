5aabfd0f07db4aa369dfa655e16bcf4c288c60bc
Byron Ponce <byron@polished-web.com>
2023-03-18 02:02:22 -0700

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,27 @@
+<script>
+ 	import { page } from '$app/stores';
+ 	import Hero from '$lib/hero/index.svelte';
+ 	import Services from '$lib/services/index.svelte';
+ 	import Reservation from '$lib/reservation/index.svelte';
+
+ 	export let data;
+ 	data.global.error.hero.title = $page.status + ': ' + $page.error.message
+ 	let servicesData = {
+		title: data.global.error.title,
+		links: data.global.nav.links
+	}
+</script>
+
+<style lang="scss">
+	@import '../style/variables.scss';
+</style>
+
+<svelte:head>
+	<title>Error</title>
+</svelte:head>
+
+<Hero data={data.global.error.hero}/>
+<main class="main">
+	<Services data={servicesData} />
+	<Reservation data="{data.global.reservation}" />
+</main>
