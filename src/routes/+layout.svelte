57c9107bd4d3c79f0810816f24dfe613f9c75492
Byron Ponce <byron@polished-web.com>
2023-03-20 02:06:02 -0700

    Pass nav config from page to nav component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
 	import Nav from '$lib/nav/index.svelte';
 	import Footer from '$lib/footer/index.svelte';
+	import { page } from '$app/stores';
 	import '../style/index.scss';
 
 	export let data;
 </script>
 
-<Nav pageName={data.pageName} data="{data.global.nav}" />
+<Nav pageName={data.pageName} data="{data.global.nav}" pageConfig={$page.data.nav}/>
 <slot />
 <Footer data={data.global.footer} />
