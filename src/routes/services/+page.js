a2cc16d9ec524356e706c257b6fb48d3718835a3
Byron Ponce <byron@polished-web.com>
2023-03-20 02:21:10 -0700

    Fix nav config error by using json data to render services page

--- src/routes/services/+page.js
+++ src/routes/services/+page.js
@@ -1,3 +1,13 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
+export const load = async ({ fetch }) => {
+	const res = await fetch('/data/services.json');
+
+	if (res.ok) {
+		return await res.json();
+	}
+
+	const { message } = await res.json();
+	
+	return {
+		error: new Error(message)
+	};
+};
--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -4,39 +4,21 @@
 	import Reservation from '$lib/reservation/index.svelte';
 
 	export let data;
-	let links = data.global.nav.links 
-	let hero = {
-		title: "Services",
-		overlay: 0.6,
-		image: {
-			ar: "16-9",
-			fit: "cover",
-			width: 1920,
-			height: 1080,
-			path: "/img/salon.jpg"
-		},
-		cta: { 
-			text: "Book an appointment",
-			url: "https://www.vagaro.com/Users/BusinessWidget.aspx?BusinessID=48649&IsPopup=0&TabsIncluded=1|99|100&b_themeID=&w_type=2&widgetversion=3&isShowMobileApp=False&tabatlaunch=3"
-		}
-	}
 	let servicesData = {
 		title: "Services",
 		links: data.global.nav.links
 	}
-	let title = "Services"
 </script>
 
 <style lang="scss">
 	@import '$lib/../style/variables.scss';
-
 </style>
 
 <svelte:head>
 	<title>Services</title>
 </svelte:head>
 
-<Hero data={hero}/>
+<Hero data={data.hero}/>
 
 <main class="main">
 	<Services data={servicesData} />
