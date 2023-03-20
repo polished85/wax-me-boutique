17601e33d63419642f7891f6a1cf3d96b4702a30
Byron Ponce <byron@polished-web.com>
2023-03-20 02:07:48 -0700

    Use page config in nav component

--- src/components/nav/index.svelte
+++ src/components/nav/index.svelte
@@ -9,11 +9,12 @@
 
 	export let data;
 	export let pageName;
+	export let pageConfig;
 	let mobileActive = false
 	let mobileActiveDropdown = null 
 	let elNode 
 	let sticky
-	let navClass = ''
+	let navSticky = ''
 	let mdBreakpoint = false
 
 	onMount(async () => {
@@ -22,23 +23,23 @@
 	});
 
 	function toggleSticky() {
-	  if (mdBreakpoint.matches && window.pageYOffset > sticky) {
-	    navClass = 'sticky'
+	  if (pageConfig.type === 'absolute' && mdBreakpoint.matches && window.pageYOffset > sticky) {
+	    navSticky = 'sticky'
 	  } else {
-	    navClass = ''
+	    navSticky = ''
 	  }
 	} 
+	
 </script>
 
 <svelte:window on:scroll={toggleSticky}/>
 
-<div class="header {pageName} {navClass}" bind:this={elNode}>
-	<div class="container-xl">
-
+<div class="header {pageName} {pageConfig.type} {pageConfig.theme} {navSticky}" bind:this={elNode}>
+	<div class="mobile-nav">
 		<div class="mobile-logo" style="background-image: url({data.logo.src});"></div>
 		<button 
 			type="button"
-			class="btn-toggle-nav {mobileActive ? "is-active" : ""}" 
+			class="btn-toggle-nav {mobileActive ? "active" : ""}" 
 			on:click={() => { mobileActive = !mobileActive }}>
 			{#if mobileActive}
 				<XIcon />
@@ -46,8 +47,9 @@
 				<MenuIcon />
 			{/if}
 		</button>
-
-		<nav>
+	</div>
+	<div class="container-xl">
+		<nav class="{mobileActive ? "active" : ""}">
 			<ul>
 				{#each data.links as link, i}
 					<li class="{link.type}" class:active='{pageName.toLowerCase() === link.name.toLowerCase()}' 
