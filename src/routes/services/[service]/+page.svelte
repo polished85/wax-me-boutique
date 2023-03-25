90f76a2d31c6065747199d180286043387722284
Byron Ponce <byron@polished-web.com>
2023-03-25 04:51:08 -0700

    Add featured section markup to services page template

--- src/routes/services/[service]/+page.svelte
+++ src/routes/services/[service]/+page.svelte
@@ -10,10 +10,23 @@
 		margin-bottom: 6rem;
 	}
 	.main {
-		padding: 6rem 0;
+		padding: 0 0 6rem;
 	}
 	.section {
 		margin-bottom: 6rem;
+	}
+	.section-featured {
+		background-color: #f4f4f3;
+		padding: 6rem 0;
+		margin: -6rem 0 6rem;
+		ol {
+			margin: 0;
+			padding: 0;
+			list-style-type: none;
+			> li {
+
+			}
+		}
 	}
 
 	@media(max-width: $screen-sm-down){
@@ -29,6 +42,29 @@
 		.text {
 			padding-left: 2rem;
 		}
+		.section-featured {
+			.title {
+				text-align: left;
+				&:after {
+					margin-left: 0;
+				}
+			}
+			.lead {
+				font-size: 2rem;
+			}
+			ol {
+				padding-left: 2rem;
+				> li {
+				text-align: left;
+					h3 {
+						text-align: left;
+						&::after {
+							margin-left: 0;
+						}
+					}
+				}
+			}
+		}
 	}
 </style>
 
@@ -42,10 +78,37 @@
 <Hero data={data.hero}/>
 
 <main class="main">
-	<div class="container">
-		<h1>{data.main.title}</h1>
-		{#each data.main.sections as section, i}
-			<div class="section">
+	{#if data.featured}
+		<section class="section-featured">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-md-5">
+						<h2 class="h1 title">{data.featured.title}</h2>
+						<p class="lead">{data.featured.lead}</p>
+					</div>
+					<div class="col-12 col-md-7">
+						<ol>
+							{#each data.featured.items as item, i}
+								<li>
+									<h3 class="h2">{item.title}</h3>
+									<p>{item.text}</p>
+								</li>
+							{/each}
+						</ol>
+					</div>
+			</div>
+		</section>
+	{/if}
+
+	{#if data.main.title}
+		<div class="container">
+			<h1>{data.main.title}</h1>
+		</div>
+	{/if}
+	
+	{#each data.main.sections as section, i}
+		<div class="section">
+			<div class="container">
 				<div class="row">
 					<div class="col-12 col-md-4">
 						<h2 class="title">{section.title}</h2>
@@ -74,8 +137,8 @@
 					</div>
 				</div>
 			</div>
-		{/each}
-	</div>
+		</div>
+	{/each}
 </main>
 
 <Reservation data="{data.global.reservation}" />
