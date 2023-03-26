8c0a6c1e632e06fc8bb8382fd6b0da8e582b0c81
Byron Ponce <byron@polished-web.com>
2023-03-26 10:42:37 -0700

    Allow html in service pages title data

--- src/routes/services/[service]/+page.svelte
+++ src/routes/services/[service]/+page.svelte
@@ -110,7 +110,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-12 col-md-4">
-						<h2 class="title">{section.title}</h2>
+						<h2 class="title">{@html section.title}</h2>
 					</div>
 					<div class="col-12 col-md-8">
 						{#if section.text}
