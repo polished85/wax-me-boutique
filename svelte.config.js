ee74342a287cb320aa0a63efcb60a86955fa671b
Byron Ponce <byron@polished-web.com>
2023-03-31 09:22:55 -0700

    Use node adapter instead of static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import node from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [vitePreprocess()],
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: node({ env: { port: process.env.PORT } }),
 		files: {
 			lib: 'src/components'
 		}
