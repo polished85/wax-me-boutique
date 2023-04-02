10795791487e1165feb724008c13a651c093cd2d
Byron Ponce <byron@polished-web.com>
2023-04-01 17:32:45 -0700

    Check that page exists before fetching page json data

--- src/routes/[page]/+page.js
+++ src/routes/[page]/+page.js
@@ -1,13 +1,21 @@
 export const load = async ({ url, params, session, fetch, stuff }) => {
-	const res = await fetch(`/data/${params.page}.json`);
 
-	if (res.ok) {
-		return await res.json();
+	var pages = [
+		'faq',
+		'policies',
+		'privacy'
+	]
+	if(pages.indexOf(params.page) !== -1){
+		const res = await fetch(`/data/${params.page}.json`);
+		if (res.ok) {
+			return await res.json();
+		} else {
+			const { message } = await res.json();
+			return {
+				error: new Error(message)
+			};
+		}
+	} else {
+		throw error(404, 'Not found');
 	}
-
-	const { message } = await res.json();
-
-	return {
-		error: new Error(message)
-	};
 };
